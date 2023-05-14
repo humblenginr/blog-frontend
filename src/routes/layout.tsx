4459aa71903dc9442babe9069e288b54fff85a15
Nithish <nithishkarthik01@gmail.com>
2023-05-15 00:40:46 +0530

    mvp
    Signed-off-by: Nithish <nithishkarthik01@gmail.com>

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,39 +1,43 @@
 import { Slot, component$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
+import { LuTwitter, LuMail, LuLinkedin } from "@qwikest/icons/lucide"
 
 export default component$(() => {
   return (
     <>
       <main class="h-screen">
-        <div class=" bg-violet-700 text-white pb-7 pt-10">
-          <div class="align flex flex-col">
-            <div class="name text-5xl">
+        <div class="bg-violet-700 text-white pb-7 pt-10">
+          <div class="px-4 align flex flex-col">
+            <a href="/" class="name text-5xl">
               Nithish Karthik
-            </div>
+            </a>
             <div class="gray flex justify-between mt-7">
               <div id="nav-links" class="flex">
-                <Link href="/blogs">
-                  <div id="nav-item" class="opacity-70 hover:opacity-100 ease-in duration-100 cursor-pointer mr-4">
-                    Blog
-                  </div>
-                </Link>
-                <Link href="/archives">
-                  <div id="nav-item" class="opacity-70 hover:opacity-100 ease-in duration-100 cursor-pointer mr-4">
-                    Archives
-                  </div>
-                </Link>
+                <a href="/blogs" id="nav-item" class="link mr-4">
+                  Blogs
+                </a>
                 <Link href="/seedlings">
-                  <div id="nav-item" class="opacity-70 hover:opacity-100 ease-in duration-100 cursor-pointer mr-4">
+                  <div id="nav-item" class="link mr-4">
                     Seedlings
                   </div>
                 </Link>
               </div>
-              <div id="social"></div>
+              <div id="social" class="flex">
+                <a href="https://twitter.com/NithishKarthi11" target="_blank" id="nav-item" class="link mr-4 text-xl">
+                  <LuTwitter />
+                </a>
+                <a href="mailto:nithishkarthik01@gmail.com" id="nav-item" class="link mr-4 text-xl">
+                  <LuMail />
+                </a>
+                <a href="https://www.linkedin.com/in/nithishkarthik/" target="_blank" id="nav-item" class="link mr-4 text-xl">
+                  <LuLinkedin />
+                </a>
+              </div>
             </div>
           </div>
         </div>
         <section>
-          <div class="align pt-8">
+          <div class="align py-8 px-4">
             <Slot />
           </div>
         </section>
