fdaae2ac0c0e1dbc333b57a584c8c6ed8766b17c
Nithish <nithishkarthik01@gmail.com>
2023-06-04 17:37:21 +0530

    feat: Add resume
    Signed-off-by: Nithish <nithishkarthik01@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,13 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity({
+      mdxPlugins: {
+        remarkGfm: true,
+        rehypeSyntaxHighlight: true,
+        rehypeAutolinkHeadings: false,
+      }
+    }), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
