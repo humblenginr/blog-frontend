b03ace2a7e62e66fc885dd37ea90b9c488393834
Nithish <nithishkarthik01@gmail.com>
2023-08-22 20:09:17 +0530

    fix: Ts error
    Signed-off-by: Nithish <nithishkarthik01@gmail.com>

--- src/components/markdown/codeblock.tsx
+++ src/components/markdown/codeblock.tsx
@@ -1,9 +1,10 @@
 /** @jsxImportSource react */
-import { CodeBlock, a11yDark, nord, tomorrowNight } from "react-code-blocks";
+import { CodeBlock, a11yDark } from "react-code-blocks";
 import { qwikify$ } from '@builder.io/qwik-react';
 
 function CB(props: { text: string, language: string }) {
   return <CodeBlock
+    // @ts-ignore
     text={props.text}
     language={props.language}
     theme={a11yDark}
